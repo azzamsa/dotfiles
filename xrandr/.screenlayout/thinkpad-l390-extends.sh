0d067771da4096d38d679edb3edddabbb3a58fbc
azzamsa <azzam@azzamsa.com>
2019-09-04 19:10:34 +0700

    Use bigger resolution xrandr

--- xrandr/.screenlayout/thinkpad-l390-extends.sh
+++ xrandr/.screenlayout/thinkpad-l390-extends.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-2 --mode 1024x768 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output eDP-1 --primary --mode 1024x768 --pos 0x768 --rotate normal --output DP-2 --off
+xrandr --output HDMI-2 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output eDP-1 --primary --mode 1366x768 --pos 0x1080 --rotate normal --output DP-2 --off
