5fb65524d83f053a0ebf764285d596c8158d0beb
azzamsa <azzam@azzamsa.com>
2019-09-04 14:00:58 +0700

    Add xrandr config

---
+++ xrandr/.screenlayout/thinkpad-l390-duplicate.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-2 --mode 1024x768 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output eDP-1 --primary --mode 1024x768 --pos 0x0 --rotate normal --output DP-2 --off
