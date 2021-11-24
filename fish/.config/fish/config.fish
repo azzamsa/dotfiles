3389110e6ee1b74eef9ee945350aac8234a97180
azzamsa <me@azzamsa.com>
2021-11-24 08:31:17 +0700

    docs: add description for each color variable in fish
    fix: remove yarn path
    
    I don't use it anymore

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,7 @@
 set fish_greeting # no greeting
 
 # Exports
-set -g fish_user_paths ~/.cargo/bin ~/bin ~/.local/bin ~/.yarn/bin $fish_user_paths
+set -g fish_user_paths ~/.cargo/bin ~/bin ~/.local/bin $fish_user_paths
 set -gx EDITOR emacs
 set -gx QT_QPA_PLATFORMTHEME qt5ct
 set -gx  BAT_THEME gruvbox-white
@@ -47,26 +47,26 @@ if test "$TERM" = "dumb"
   function fish_title; end
 end
 
-# Scheme (Paper Color)
-set -U fish_color_autosuggestion      005f87
-set -U fish_color_cancel              af0000
-set -U fish_color_command             312E81
-set -U fish_color_comment             5f8700
-set -U fish_color_end                 0087af
-set -U fish_color_error               878787
-set -U fish_color_escape              444444
-set -U fish_color_history_current     bcbcbc
-set -U fish_color_match               d70000
+# Color scheme
+set -U fish_color_error               4338CA # #4338CA default color when typing suggestion
+set -U fish_color_autosuggestion      6B7280 # #6B7280 suggestion
+set -U fish_color_command             D97706 # #D97706 binary command
+set -U fish_color_quote               5f8700 # #5f8700 text color inside the quote
+set -U fish_color_comment             5f8700 # #5f8700 comment color
+set -U fish_color_valid_path          --underline # attribute for a valid path
+set -U fish_color_param               8700af # #8700af paremeter color such `--version`
+set -U fish_color_cancel              af0000 # #af0000
+set -U fish_color_end                 0087af # #0087af
+set -U fish_color_escape              444444 # #444444
+set -U fish_color_history_current     F59E0B # #F59E0B
+set -U fish_color_match               d70000 # #d70000
 set -U fish_color_normal              normal
-set -U fish_color_operator            d70087
-set -U fish_color_param               8700af
-set -U fish_color_quote               d75f00
-set -U fish_color_redirection         005faf
-set -U fish_color_search_match        005f87
+set -U fish_color_operator            d70087 # #d70087
+set -U fish_color_redirection         005faf # #005faf
+set -U fish_color_search_match        005f87 # #005f87
 set -U fish_color_selection           'white' '--bold' '--background=brblack'
 set -U fish_color_status              red
-set -U fish_color_valid_path          --underline
-set -U fish_pager_color_completion    0087af
+set -U fish_pager_color_completion    0087af # #0087af
 set -U fish_pager_color_description   yellow
 set -U fish_pager_color_prefix        'white' '--bold' '--underline'
 set -U fish_pager_color_progress      'brwhite' '--background=cyan'
