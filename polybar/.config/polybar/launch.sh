22d7f35a420eb308c5db56d9d956e7ade1d2bf41
azzamsa <me@azzamsa.com>
2021-12-06 17:59:28 +0700

    refactor: follow bash best practice

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -10,9 +10,9 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 active_monitors=$(xrandr | awk '/ connected/ && /[[:digit:]]x[[:digit:]].*+/{print $1}')
 
 if type "xrandr"; then
-    for m in $active_monitors; do
-        echo "::: setup monitor: $m"
-        MONITOR=$m polybar --reload bar1 &
+    for monitor in $active_monitors; do
+        echo "::: setup monitor: $monitor"
+        MONITOR="$monitor" polybar --reload bar1 &
     done
 else
     polybar --reload bar1 &
