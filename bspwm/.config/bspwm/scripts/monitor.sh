e56719975389c8cf6065788b39f3c4e56f19aad7
azzamsa <me@azzamsa.com>
2021-12-06 09:56:50 +0700

    feat: support external monitors

---
+++ bspwm/.config/bspwm/scripts/monitor.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash
+# credits:  criswell/meh
+
+active_monitors=$(xrandr | awk '/\ connected/ && /[[:digit:]]x[[:digit:]].*+/{print $1}')
+
+unset DISPLAY_LAPTOP DISPLAY_HDMI || true
+
+for m in $active_monitors
+do
+    if [[ "$m" == "eDP-1" ]]; then
+        DISPLAY_LAPTOP=yes
+    elif [[ "$m" == "HDMI-1" ]]; then
+        DISPLAY_HDMI=yes
+    fi
+done
+
+layout_laptop_only() {
+    bspc monitor -d 1 2 3 4
+}
+
+layout_external_hdmi_only() {
+    bspc monitor HDMI-1 -d 1 2 3 4
+}
+
+layout_external_all() {
+    bspc monitor eDP-1 -d  1
+    bspc monitor HDMI-1 -d 2 3 4 5
+}
+
+if [ -n "$DISPLAY_LAPTOP" ] && [ -n "$DISPLAY_HDMI" ]; then
+    echo "::: All monitors are active"
+    layout_external_all
+elif [ -n "$DISPLAY_HDMI" ]; then
+    echo "::: Only HDMI is active"
+    layout_external_hdmi_only
+else
+    echo "::: Only LAPTOP is active"
+    layout_laptop_only
+fi
--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,12 +6,15 @@ pkill polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+active_monitors=$(xrandr | awk '/\ connected/ && /[[:digit:]]x[[:digit:]].*+/{print $1}')
+
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload bar1 &
-  done
+    for m in $active_monitors; do
+        echo "::: setup monitor: $m"
+        MONITOR=$m polybar --reload bar1 &
+    done
 else
-  polybar --reload bar1 &
+    polybar --reload bar1 &
 fi
 
 echo "Bars launched..."
