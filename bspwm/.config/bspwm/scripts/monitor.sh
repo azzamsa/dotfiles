f6e02f183535ec4e923b2a106d0945bc7fd74be5
azzamsa <me@azzamsa.com>
2021-12-06 15:58:48 +0700

    refactor: avoid duplicate code in managing multiple screen

--- bspwm/.config/bspwm/scripts/monitor.sh
+++ bspwm/.config/bspwm/scripts/monitor.sh
@@ -1,36 +1,26 @@
 #!/usr/bin/bash
 
-#
-# workspaces
-#
+active_monitors=$(xrandr | awk '/ connected/ && /[[:digit:]]x[[:digit:]].*+/{print $1}')
+echo "::: active monitors: $active_monitors"
 
-# Need to move all the workspace to second montior
-# Otherwise, I can't invoke `super-4` to get to workspsace 1.
-# credits: https://github.com/ericmurphyxyz/archrice/blob/6e85165338a6e35503ece16025850412bc4768b2/.local/bin/startup
-move_workspace_to_external_monitor() {
-    internal_monitor=eDP-1
-    external_monitor=HDMI-1
+# This will only gets you ONE active HDMI monitor
+# I use this becase I have two laptop connected to one monitor
+# So they have HDMI-1 and HDMI-2 connection name respectively
+# Please change the code below if you have different requirements
+active_hdmi_monitor=$(echo "$active_monitors" |  awk '/HDMI-[[:digit:]]/ {print $1}' )
+echo "::: active HDMI monitors: $active_hdmi_monitor"
 
-    desktops=1 # How many desktops to move to the second monitor
-
-    for desktop in $(bspc query -D -m $internal_monitor | sed "$desktops"q)
-    do
-        bspc desktop $desktop --to-monitor $external_monitor
-    done
-
-    # Remove "Desktop" created by bspwm
-    bspc desktop Desktop --remove
-}
-
+builtin_monitor=eDP-1
+workspace_count=1 # How many desktops to move to the second monitor
 
 #
 # brightness
 #
 
 is_night(){
-    hour=`date +%H`
+    hour=$(date +%H)
 
-    if [ $hour -lt 18 ]; then
+    if [ "$hour" -lt 17 ]; then
         echo "::: It is a day time"
         return 1
     else
@@ -39,66 +29,77 @@ is_night(){
     fi
 }
 
+set_hdmi_brightness(){
+    if is_night; then
+        xrandr --output "$active_hdmi_monitor" --brightness 0.60 --gamma 1.1:0.8:0.7
+    else
+        xrandr --output "$active_hdmi_monitor" --brightness 0.75 --gamma 0:0:0
+    fi
+}
+
+set_builtin_brightness(){
+    if is_night; then
+        light -S 15
+    else
+        light -S 30
+    fi
+}
+
+
+#
+# workspaces
+#
+
+# Need to move all the workspace to second montior
+# Otherwise, I can't invoke `super-4` to get to workspsace 1.
+move_workspace_to_external_monitor() {
+    desktops_in_builtin_monitor=$(bspc query -D -m "$builtin_monitor" | sed "$workspace_count"q)
+
+    for desktop in $desktops_in_builtin_monitor; do
+        bspc desktop "$desktop" --to-monitor "$active_hdmi_monitor"
+    done
+
+    # Remove "Desktop" created by bspwm
+    bspc desktop Desktop --remove
+}
+
 
 
 #
 # monitors
 #
-# credits: https://github.com/criswell/meh/blob/0745d064c2219d22d98f8a6502846698938156ff/bijaz-theme/.config/bspwm/monitor.sh
-
-active_monitors=$(xrandr | awk '/\ connected/ && /[[:digit:]]x[[:digit:]].*+/{print $1}')
 
 unset DISPLAY_LAPTOP DISPLAY_HDMI || true
 
-for m in $active_monitors
+for monitor in $active_monitors
 do
-    if [[ "$m" == "eDP-1" ]]; then
+    if [[ "$monitor" == "eDP-1" ]]; then
         DISPLAY_LAPTOP=yes
-    elif [[ "$m" == "HDMI-1" ]]; then
+    elif [[ "$monitor" == "HDMI-1" ]]; then
+        DISPLAY_HDMI=yes
+    elif [[ "$monitor" == "HDMI-2" ]]; then
+        # HDMI-2 is when both my laptop connected
+        # to one external monitor
         DISPLAY_HDMI=yes
     fi
 done
 
 layout_laptop_only() {
-    # bspc monitor -d 4 5 6 7
-    bspc monitor -d 1 2 3 4 6 7
-
-    if  is_night; then
-        light -S 15
-    else
-        light -S 30
-    fi
+    bspc monitor -d 4 5 6 7
+    set_builtin_brightness
 }
 
 layout_external_hdmi_only() {
-    # bspc monitor HDMI-1 -d 4 5 6 7
-    # TODO How to start at specific monitor?
-    bspc monitor HDMI-1 -d 1 2 3 4 5 6 7
-
-    if is_night; then
-        xrandr --output HDMI-1 --brightness 0.60 --gamma 1.1:0.8:0.7
-    else
-        xrandr --output HDMI-1 --brightness 0.75 --gamma 0:0:0
-    fi
+    bspc monitor "$active_hdmi_monitor" -d 1 2 3 4 5 6 7
+    set_hdmi_brightness
 }
 
 layout_external_all() {
-    # bspwm always assing to source monitor first
-    bspc monitor eDP-1 -d 1
-    bspc monitor HDMI-1 -d 2 3 4 5 6 7
+    bspc monitor "$builtin_monitor" -d 1
+    bspc monitor "$active_hdmi_monitor" -d 2 3 4 5 6 7
 
-    # TODO avoid duplication
-    if  is_night; then
-        light -S 15
-    else
-        light -S 30
-    fi
-
-    if is_night; then
-        xrandr --output HDMI-1 --brightness 0.60 --gamma 1.1:0.8:0.7
-    else
-        xrandr --output HDMI-1 --brightness 0.75 --gamma 0:0:0
-    fi
+    set_builtin_brightness
+    set_hdmi_brightness
 }
 
 if [ -n "$DISPLAY_LAPTOP" ] && [ -n "$DISPLAY_HDMI" ]; then
@@ -112,3 +113,4 @@ else
     echo "::: Only LAPTOP is active"
     layout_laptop_only
 fi
+
--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,7 +6,8 @@ pkill polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-active_monitors=$(xrandr | awk '/\ connected/ && /[[:digit:]]x[[:digit:]].*+/{print $1}')
+# https://wiki.archlinux.org/title/xrandr
+active_monitors=$(xrandr | awk '/ connected/ && /[[:digit:]]x[[:digit:]].*+/{print $1}')
 
 if type "xrandr"; then
     for m in $active_monitors; do
