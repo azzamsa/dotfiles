2dead77cf86361a1fa633faf6864685fe908d1eb
azzamsa <me@azzamsa.com>
2021-12-13 15:05:03 +0700

    feat(polybar): use env variable for wlan interface name

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -7,12 +7,14 @@ pkill polybar
 # while pgrep -u "$(UID)" -x polybar >/dev/null; do sleep 1; done
 
 # https://wiki.archlinux.org/title/xrandr
-active_monitors=$(xrandr | awk '/ connected/ && /[[:digit:]]x[[:digit:]].*+/{print $1}')
+ACTIVE_MONITORS=$(xrandr | awk '/ connected/ && /[[:digit:]]x[[:digit:]].*+/{print $1}')
+
+WLAN_INTERFACE=$(ip -o link show | awk '/wl/ {print substr($2, 0, length($2)-1)}')
 
 if type "xrandr"; then
-    for monitor in $active_monitors; do
+    for monitor in $ACTIVE_MONITORS; do
         echo "::: setup monitor: $monitor"
-        MONITOR="$monitor" polybar --reload bar1 &
+        MONITOR="$monitor" WLAN_INTERFACE="$WLAN_INTERFACE" polybar --reload bar1 &
     done
 else
     polybar --reload bar1 &
