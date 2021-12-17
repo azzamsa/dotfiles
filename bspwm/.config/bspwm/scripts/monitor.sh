a8292be95c62967e28f24b3de04414eb96a69df4
azzamsa <me@azzamsa.com>
2021-12-17 07:49:32 +0700

    fix(bspwm): running the monitor.sh in bspwmrc cause it to restart indefinitely
    The solution is to run it from xinitrc and wait until bspwm settled
    before loading autorandr profile.

--- bspwm/.config/bspwm/scripts/monitor.sh
+++ bspwm/.config/bspwm/scripts/monitor.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/sh
 
-IS_HDMI_CONNECTED=$(bspc query --monitors --names | grep HDMI)
+# Wait until bspwm settled
+sleep 5
+
+# Active monitor example output: `eDP-1 HDMI-2`
+ACTIVE_MONITORS=$(xrandr --listactivemonitors | grep -v Monitors | grep -Eo '[^ ]+$')
+
+ACTIVE_HDMI_MONITOR=$(echo "$ACTIVE_MONITORS" | awk '/HDMI/ {print $1}')
 
 # If there is the HDMI connection. Load the docked profile
 # Most of the time, I don't use my laptop screen.
 # For presentation, I can just fire up the `--load demo` command
-if [ -n "$IS_HDMI_CONNECTED" ]; then
+if [ -n "$ACTIVE_HDMI_MONITOR" ]; then
     autorandr --load docked
 fi
