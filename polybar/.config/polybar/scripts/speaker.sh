5bd85c996241b092996976f65585cade225a22a1
azzamsa <me@azzamsa.com>
2021-12-26 10:46:30 +0700

    feat(polybar): manage volume level from the bar

--- polybar/.config/polybar/scripts/speaker.sh
+++ polybar/.config/polybar/scripts/speaker.sh
@@ -6,7 +6,8 @@ capture_numid=$(amixer controls |  awk -F , '/Master Playback Switch/ {print $1}
 speaker_status=$(amixer cget numid="$capture_numid" | awk '/:/ {print $2}' |  awk -F = '{print $2}')
 
 if [ "$speaker_status" = "on" ]; then
-    echo ""
+    current_volume=$(amixer sget Master | awk -F '[][]' '/Left:/ { print $2 }')
+    echo ": $current_volume"
 else
     echo ""
 fi
