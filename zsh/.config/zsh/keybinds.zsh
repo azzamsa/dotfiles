605f48f2843d4b68572b8ee7e3e29dc5ecbddb7b
azzamsa <me@azzamsa.com>
2022-09-12 09:15:25 +0700

    fix(zsh): I don't use tmux
    Wezterm has built-in panes.

--- zsh/.config/zsh/keybinds.zsh
+++ zsh/.config/zsh/keybinds.zsh
@@ -28,29 +28,6 @@ if (( $+commands[fzf] )); then
   bindkey '^R' fzf-history-widget
 fi
 
-# Completing words in buffer in tmux
-if [ -n "$TMUX" ]; then
-  _tmux_pane_words() {
-    local expl
-    local -a w
-    if [[ -z "$TMUX_PANE" ]]; then
-      _message "not running inside tmux!"
-      return 1
-    fi
-    w=( ${(u)=$(tmux capture-pane \; show-buffer \; delete-buffer)} )
-    _wanted values expl 'words from current tmux pane' compadd -a w
-  }
-
-  zle -C tmux-pane-words-prefix   complete-word _generic
-  zle -C tmux-pane-words-anywhere complete-word _generic
-
-  bindkey -M viins '^x^n' tmux-pane-words-prefix
-  bindkey -M viins '^x^o' tmux-pane-words-anywhere
-
-  zstyle ':completion:tmux-pane-words-(prefix|anywhere):*' completer _tmux_pane_words
-  zstyle ':completion:tmux-pane-words-(prefix|anywhere):*' ignore-line current
-  zstyle ':completion:tmux-pane-words-anywhere:*' matcher-list 'b:=* m:{A-Za-z}={a-zA-Z}'
-fi
 
 # Vim's C-x C-l in zsh
 history-beginning-search-backward-then-append() {
