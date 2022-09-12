9e30167efdf361bb55eac7fe23e93c5937774ce7
azzamsa <me@azzamsa.com>
2022-09-12 09:40:48 +0700

    fix(zsh): weird behavior without explicit `HISTFILE` location

--- zsh/.config/zsh/config.zsh
+++ zsh/.config/zsh/config.zsh
@@ -49,6 +49,7 @@ unsetopt HUP              # Don't kill jobs on shell exit.
 unsetopt CHECK_JOBS       # Don't report on jobs when shell exit.
 
 ## History
+HISTFILE=~/.zsh_history
 HISTSIZE=100000   # Max events to store in internal history.
 SAVEHIST=100000   # Max events to store in history file.
 
