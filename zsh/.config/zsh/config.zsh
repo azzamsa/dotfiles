82866001242ce5c735a27008a7464f07fcdf82a1
azzamsa <me@azzamsa.com>
2022-09-16 09:13:53 +0700

    fix(zsh): don't highlight paste
    I can't see my cursor.

--- zsh/.config/zsh/config.zsh
+++ zsh/.config/zsh/config.zsh
@@ -76,3 +76,6 @@ setopt MULTIOS              # Write to multiple descriptors.
 setopt EXTENDED_GLOB        # Use extended globbing syntax.
 unsetopt GLOB_DOTS
 unsetopt AUTO_NAME_DIRS     # Don't add variable-stored paths to ~ list
+
+# ZLE
+zle_highlight=('paste:none')
