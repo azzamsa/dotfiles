6dc9d319cae13f79db0a8b1004aa87b103520415
azzamsa <me@azzamsa.com>
2023-03-15 04:17:38 +0700

    refactor: some app can't be installed in hidden directory

--- zsh/.config/zsh/paths.zsh
+++ zsh/.config/zsh/paths.zsh
@@ -2,8 +2,8 @@
 
 path=(
   ~/.local/bin(N)
-  ~/.opt/bin/{private,public}(N)
-  ~/.opt/nodebin/node_modules/.bin(N)
+  ~/opt/bin/{private,public}(N)
+  ~/opt/nodebin/node_modules/.bin(N)
   "$XDG_CONFIG_HOME"/emacs/bin(N)
   "$XDG_DATA_HOME"/cargo/bin(N)
   $path
