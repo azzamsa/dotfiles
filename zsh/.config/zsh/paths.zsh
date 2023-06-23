3b8c8d99d46e54163e2000253cdbbef38b1ae2ee
azzamsa <me@azzamsa.com>
2023-06-24 06:59:39 +0700

    fix(zsh): I don't use Doom anymore.
    I hate when doing `doom upgrade` and it breaks.
    It breaks in critical times, and I don't get help in Discrod.
    Yes, it is silly to run upgrade during critical times.

--- zsh/.config/zsh/paths.zsh
+++ zsh/.config/zsh/paths.zsh
@@ -4,7 +4,6 @@ path=(
   "$HOME"/opt/bin(N)
   "$HOME"/.local/bin(N)
   "$HOME"/opt/nodebin/node_modules/.bin(N)
-  "$XDG_CONFIG_HOME"/emacs/bin(N)
   "$XDG_DATA_HOME"/cargo/bin(N)
   "$XDG_DATA_HOME"/rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin(N)
   "$HOME"/.rye/shims(N)
