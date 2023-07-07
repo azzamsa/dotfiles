e9112092815cf0bafe661a1928b8bbfe733f02f0
azzamsa <me@azzamsa.com>
2023-07-07 17:37:52 +0700

    feat(xtool): choose any module to run

--- xtool/src/clean.rs
+++ xtool/src/clean.rs
@@ -1,7 +1,26 @@
-use std::{env, fs};
+use std::{env, ffi::OsString, fs};
 
 use duct::cmd;
 use serde::Deserialize;
+
+const HELP: &str = "\
+clean
+
+USAGE:
+  clean [MODULES]
+
+MODULES:
+  kondo pm flatpak container tmp 
+";
+
+#[derive(Debug)]
+enum Modules {
+    Kondo,
+    PackageManager,
+    Flatpak,
+    Container,
+    Tmp,
+}
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "PascalCase")]
@@ -11,24 +30,77 @@ struct Image {
 }
 
 pub(crate) fn run() -> anyhow::Result<()> {
-    let mut args = pico_args::Arguments::from_env();
-    let target: Option<String> = args.opt_free_from_str()?;
+    let mut args: Vec<_> = std::env::args_os().collect();
+    args.remove(0); // remove the executable path.
+    let mut args = pico_args::Arguments::from_vec(args);
 
-    match target {
-        None => all()?,
-        Some(t) => match t.as_ref() {
-            "tmp" => tmp()?,
-            _ => anyhow::bail!("unknown target: `{}`", t),
-        },
+    // Help has a higher priority and should be handled separately.
+    if args.contains(["-h", "--help"]) {
+        print!("{}", HELP);
+        std::process::exit(0);
     }
-    println!("✨ You have a new shiny machine!");
 
+    let mut valid_mods: Vec<Modules> = Vec::new();
+    let mut invalid_mods: Vec<String> = Vec::new();
+
+    let remaining = args.finish();
+    if !remaining.is_empty() {
+        parse_modules(remaining, &mut valid_mods, &mut invalid_mods)?;
+        execute(valid_mods, invalid_mods)?;
+    } else {
+        all()?;
+    }
+
+    println!("✨ You have a new shiny machine!");
+    Ok(())
+}
+
+fn execute(valid_mods: Vec<Modules>, invalid_mods: Vec<String>) -> anyhow::Result<()> {
+    if !invalid_mods.is_empty() {
+        anyhow::bail!("Unknown modules `{}`", &invalid_mods.join(", "))
+    } else {
+        for module in valid_mods {
+            match module {
+                Modules::Kondo => kondo()?,
+                Modules::PackageManager => package_manager()?,
+                Modules::Flatpak => flatpak()?,
+                Modules::Container => container()?,
+                Modules::Tmp => tmp()?,
+            }
+        }
+    }
+    Ok(())
+}
+
+fn parse_modules(
+    modules: Vec<OsString>,
+    valid_mods: &mut Vec<Modules>,
+    invalid_mods: &mut Vec<String>,
+) -> anyhow::Result<()> {
+    for module in modules {
+        match module.to_string_lossy().to_string().to_lowercase().as_str() {
+            "kondo" => valid_mods.push(Modules::Kondo),
+            "pm" => valid_mods.push(Modules::PackageManager),
+            "flatpak" => valid_mods.push(Modules::Flatpak),
+            "container" => valid_mods.push(Modules::Container),
+            "tmp" => valid_mods.push(Modules::Tmp),
+            _ => invalid_mods.push(module.to_string_lossy().to_string()),
+        }
+    }
     Ok(())
 }
 
 fn all() -> anyhow::Result<()> {
-    let home = env::var("HOME")?;
+    kondo()?;
+    package_manager()?;
+    flatpak()?;
+    container()?;
     tmp()?;
+    Ok(())
+}
+
+fn kondo() -> anyhow::Result<()> {
+    let home = env::var("HOME")?;
 
     println!("🧽 Cleaning dependencies and build artifacts");
     cmd!(
@@ -41,13 +113,22 @@ fn all() -> anyhow::Result<()> {
     )
     .unchecked()
     .run()?;
+    Ok(())
+}
 
-    println!("🧽 Cleaning unused layer");
+fn package_manager() -> anyhow::Result<()> {
+    println!("🧽 Cleaning package manager cache");
     cmd!("sudo", "dnf", "autoremove").run()?;
+    Ok(())
+}
 
+fn flatpak() -> anyhow::Result<()> {
     println!("🧽 Cleaning unused flatpak package");
     cmd!("flatpak", "uninstall", "--unused").unchecked().run()?;
+    Ok(())
+}
 
+fn container() -> anyhow::Result<()> {
     println!("🧽 Cleaning container cache");
     let images = cmd!("podman", "images", "--all", "--format=json").read()?;
     let images: Vec<Image> = serde_json::from_str(&images)?;
@@ -56,7 +137,6 @@ fn all() -> anyhow::Result<()> {
             cmd!("podman", "rmi", &image.id, "--force").run()?;
         }
     }
-
     Ok(())
 }
 
--- xtool/src/upgrade.rs
+++ xtool/src/upgrade.rs
@@ -1,29 +1,114 @@
-use std::env;
+use std::{env, ffi::OsString};
 
 use duct::cmd;
 
-pub(crate) fn run() -> anyhow::Result<()> {
-    exec()?;
-    println!("✨ You have a new shiny machine!");
+const HELP: &str = "\
+upgrade
 
+USAGE:
+  upgrade [MODULES]
+
+MODULES:
+  flatpak pm npm cargo
+";
+
+#[derive(Debug)]
+enum Modules {
+    Flatpak,
+    PackageManager,
+    Npm,
+    Cargo,
+}
+
+pub(crate) fn run() -> anyhow::Result<()> {
+    let mut args: Vec<_> = std::env::args_os().collect();
+    args.remove(0); // remove the executable path.
+    let mut args = pico_args::Arguments::from_vec(args);
+
+    // Help has a higher priority and should be handled separately.
+    if args.contains(["-h", "--help"]) {
+        print!("{}", HELP);
+        std::process::exit(0);
+    }
+
+    let mut valid_mods: Vec<Modules> = Vec::new();
+    let mut invalid_mods: Vec<String> = Vec::new();
+
+    let remaining = args.finish();
+    if !remaining.is_empty() {
+        parse_modules(remaining, &mut valid_mods, &mut invalid_mods)?;
+        execute(valid_mods, invalid_mods)?;
+    } else {
+        all()?;
+    }
+
+    println!("✨ You have a new shiny machine!");
     Ok(())
 }
 
-fn exec() -> anyhow::Result<()> {
+fn execute(valid_mods: Vec<Modules>, invalid_mods: Vec<String>) -> anyhow::Result<()> {
+    if !invalid_mods.is_empty() {
+        anyhow::bail!("Unknown modules `{}`", &invalid_mods.join(", "))
+    } else {
+        for module in valid_mods {
+            match module {
+                Modules::Flatpak => flatpak()?,
+                Modules::PackageManager => package_manager()?,
+                Modules::Npm => npm()?,
+                Modules::Cargo => cargo()?,
+            }
+        }
+    }
+    Ok(())
+}
+
+fn parse_modules(
+    modules: Vec<OsString>,
+    valid_mods: &mut Vec<Modules>,
+    invalid_mods: &mut Vec<String>,
+) -> anyhow::Result<()> {
+    for module in modules {
+        match module.to_string_lossy().to_string().to_lowercase().as_str() {
+            "flatpak" => valid_mods.push(Modules::Flatpak),
+            "pm" => valid_mods.push(Modules::PackageManager),
+            "npm" => valid_mods.push(Modules::Npm),
+            "cargo" => valid_mods.push(Modules::Cargo),
+            _ => invalid_mods.push(module.to_string_lossy().to_string()),
+        }
+    }
+    Ok(())
+}
+
+fn all() -> anyhow::Result<()> {
+    flatpak()?;
+    package_manager()?;
+    npm()?;
+    cargo()?;
+    Ok(())
+}
+
+fn flatpak() -> anyhow::Result<()> {
     println!("🌱 Updating flatpak apps");
     cmd!("flatpak", "update").unchecked().run()?;
+    Ok(())
+}
 
-    println!("🌱 Upgrading system");
+fn package_manager() -> anyhow::Result<()> {
+    println!("🌱 Upgrading OS package manager apps");
     cmd!("sudo", "dnf", "upgrade").unchecked().run()?;
+    Ok(())
+}
 
-    println!("🌱 Checking node apps");
+fn npm() -> anyhow::Result<()> {
+    println!("🌱 Checking npm apps");
     let dir = format!("{}/opt/nodebin", env::var("HOME")?);
     cmd!("taze", "major", "--write").dir(&dir).run()?;
     cmd!("npm", "install").dir(&dir).run()?;
+    Ok(())
+}
 
-    println!("🌱 Checking rust apps");
-    cmd!("rustup", "check").run()?;
+fn cargo() -> anyhow::Result<()> {
+    println!("🌱 Checking cargo apps");
     cmd!("cargo", "install-update", "-a").run()?;
-
     Ok(())
 }
