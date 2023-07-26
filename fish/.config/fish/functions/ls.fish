dc5a1dc295455f71b94b0121d971135acd407893
azzamsa <me@azzamsa.com>
2023-07-26 17:16:41 +0700

    fix(fish): old habit die hard

--- fish/.config/fish/functions/ls.fish
+++ fish/.config/fish/functions/ls.fish
@@ -2,6 +2,10 @@ function l
     exa --group-directories-first --git $argv
 end
 
+function ls
+    exa --group-directories-first --git $argv
+end
+
 function la
     exa --group-directories-first --git --all $argv
 end
