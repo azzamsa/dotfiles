bf4a6c9c5f6583b01ad14668e9e3e9a9c8b9f8fa
azzamsa <me@azzamsa.com>
2023-08-27 19:52:04 +0700

    fix(xtool): Remove the 'flatpak' and 'package-manager' upgrade steps
    Nowadays, I have an optical cable connection and no longer need to worry about internet quotas.

--- xtool/src/upgrade.rs
+++ xtool/src/upgrade.rs
@@ -1,5 +1,4 @@
 use std::env;
-use std::fs;
 
 use clap::{Parser, ValueEnum};
 use duct::cmd;
@@ -14,9 +13,6 @@ pub struct Opts {
 
 #[derive(Debug, Clone, ValueEnum)]
 pub enum Module {
-    Flatpak,
-    #[clap(name = "pm")]
-    PackageManager,
     Npm,
     Cargo,
 }
@@ -36,8 +32,6 @@ pub(crate) fn run() -> anyhow::Result<()> {
 fn execute(modules: Vec<Module>) -> anyhow::Result<()> {
     for module in modules {
         match module {
-            Module::Flatpak => flatpak()?,
-            Module::PackageManager => package_manager()?,
             Module::Npm => npm()?,
             Module::Cargo => cargo()?,
         }
@@ -46,41 +40,8 @@ fn execute(modules: Vec<Module>) -> anyhow::Result<()> {
 }
 
 fn all() -> anyhow::Result<()> {
-    flatpak()?;
-    package_manager()?;
     npm()?;
     cargo()?;
-    Ok(())
-}
-
-fn flatpak() -> anyhow::Result<()> {
-    println!("📥 Updating flatpak apps");
-    cmd!(
-        "flatpak",
-        "update",
-        "io.gitlab.news_flash.NewsFlash",
-        "com.github.johnfactotum.Foliate",
-        "com.logseq.Logseq",
-        "org.atheme.audacious",
-        "org.keepassxc.KeePassXC",
-        "org.mozilla.Thunderbird",
-        "rest.insomnia.Insomnia",
-        "--no-related"
-    )
-    .unchecked()
-    .run()?;
-    Ok(())
-}
-
-fn package_manager() -> anyhow::Result<()> {
-    println!("📥 Upgrading OS package manager apps");
-    let package_manager = get_package_manager_name();
-    match package_manager {
-        None => anyhow::bail!("built-in package manager not found"),
-        Some(pm) => {
-            cmd!("sudo", pm, "upgrade").unchecked().run()?;
-        }
-    }
     Ok(())
 }
 
@@ -96,32 +57,4 @@ fn cargo() -> anyhow::Result<()> {
     println!("📥 Checking cargo apps");
     cmd!("cargo", "install-update", "-a").run()?;
     Ok(())
-}
-
-fn get_package_manager_name() -> Option<String> {
-    match get_os_name() {
-        None => None,
-        Some(name) => {
-            if name.contains("Fedora") {
-                Some("dnf".to_string())
-            } else if name.contains("Debian") {
-                Some("apt".to_string())
-            } else {
-                None
-            }
-        }
-    }
-}
-
-fn get_os_name() -> Option<String> {
-    if let Ok(contents) = fs::read_to_string("/etc/os-release") {
-        for line in contents.lines() {
-            if let Some(name) = line.strip_prefix("NAME=") {
-                // Extract the name value and remove quotes if present
-                let name = name.trim().trim_matches('"').to_string();
-                return Some(name);
-            }
-        }
-    }
-    None
 }
