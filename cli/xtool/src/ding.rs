24a565f1f3225258ffbeb1209c163202f9f5337a
azzamsa <me@azzamsa.com>
2024-08-01 15:07:23 +0700

    refactor(scripts): migrate xtool to scripts
    - cargo script is now getting better.
    - nushell is just a better and faster tool to write simple scripts.

--- cli/xtool/src/ding.rs
+++
@@ -1,8 +0,0 @@
-use duct::cmd;
-use std::env;
-
-pub(crate) fn run() -> anyhow::Result<()> {
-    let target = format!("{}/music/notifications/ding.ogg", env::var("HOME")?);
-    cmd!("pw-play", target).run()?;
-    Ok(())
-}
