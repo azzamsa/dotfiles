48ae8cbec4f0dd24219deb782253db275e7e2222
Azzam S.A <noreply@azzamsa.com>
2024-12-11 05:33:47 +0700

    style(yazi): use flavor instead of theme
    It is cleaner and more powerful

--- .dotter/post_deploy.sh
+++ .dotter/post_deploy.sh
@@ -1,7 +1,1 @@
 #!/bin/bash
-
-# Extract the theme value and save it to a variable
-theme=$(grep '^theme = ' ~/dotfiles/.dotter/local.toml | cut -d '"' -f2)
-
-# Copy the theme file to the desired location
-cp -f ~/dotfiles/utils/yazi/themes/"$theme".toml ~/.config/yazi/theme.toml
