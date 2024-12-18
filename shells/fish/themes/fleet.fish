f0faaabd3bd4f6726b4aed0e923710ea9e8b4d66
Azzam S.A <noreply@azzamsa.com>
2024-12-18 21:17:50 +0700

    style: add `fleet`

---
+++ shells/fish/themes/fleet.fish
@@ -0,0 +1,17 @@
+# Tested on black background
+# Source:
+# - https://github.com/helix-editor/helix/blob/master/runtime/themes/fleet_dark.toml#L1
+# - https://github.com/nylar/fleetish-emacs-theme/blob/master/fleetish-theme.el
+
+set -g foreground ffffff # 21:1, #ffffff
+set -g selection 44475a #, #163764
+set -g comment 898989 # , #898989
+
+set -g red cc7c8a # 6.81:1 (AA), #cc7c8a
+set -g orange e09b70 # 9.09:1, #e09b70
+set -g yellow ebc88d # 13.17:1, #ebc88d
+set -g green a8cc7c # 11.59:1, #a8cc7c
+set -g purple af9cff # 9.02:1, #af9cff
+set -g cyan 82d2ce # 12.05:1, #82d2ce
+set -g pink e394dc # 9.53:1, #e394dc
+# blue = "#87c3ff"
