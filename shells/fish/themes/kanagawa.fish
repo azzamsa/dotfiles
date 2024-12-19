cfe3c4cdc2c30c9486a6f0d48fbcb1d037106aab
Azzam S.A <noreply@azzamsa.com>
2024-12-19 18:16:05 +0700

    style: match the color choice to upstream kanagawa

--- shells/fish/themes/kanagawa.fish
+++ shells/fish/themes/kanagawa.fish
@@ -1,19 +1,21 @@
-# Ayu dark.
-#
 # Tested on black background
 # Source:
-# - https://github.com/ayu-theme/ayu-vim/blob/master/colors/ayu.vim
+# - https://github.com/rebelot/kanagawa.nvim/blob/master/extras/kanagawa.fish
 
 set -g foreground dcd7ba # 16.03:1, #dcd7ba
-set -g selection 223249 #, #223249
+set -g selection #2d4f67 #, #2d4f67
 set -g comment 727169 # , #727169
 
-set -g red d27e99 # 7.18:1, #d27e99
-set -g orange ffa066 # 10.47:1, #ffa066
-set -g yellow e6c384 # 12.5:1, #e6c384
-set -g green 98bb6c # 9.65:1, #98bb6c
-set -g purple 7e9cd8 # 7.63:1, #7e9cd8
-# too stand out from others
-# set -g cyan a3d4d5 # 12.93:1, #a3d4d5
-set -g cyan 7aa89f # 9.28:1, #7fb4ca
+set -g red c34043 # 4.13:1 (!), #c34043
+set -g orange ff9e64 # 10.32:1, #ff9e64
+set -g yellow c0a36e # 8.7:1, #c0a36e
+set -g green 76946a # 6.21:1, #76946a
+set -g purple 957fb8 # 6:1 (AA), #957fb8
+set -g cyan 7aa89f # 7.92:1, #7aa89f
 set -g pink d27e99 # 7.18:1, #d27e99
+
+# mine
+# set -g orange ffa066 # 10.47:1, #ffa066
+# set -g yellow e6c384 # 12.5:1, #e6c384
+# set -g green 98bb6c # 9.65:1, #98bb6c
+# set -g purple 7e9cd8 # 7.63:1, #7e9cd8
