108b7840f81ca0d6db00f78968427a57c94c418f
Azzam S.A <noreply@azzamsa.com>
2024-12-24 05:26:11 +0700

    style: carbonfox

---
+++ shells/fish/themes/oxocarbon.fish
@@ -0,0 +1,16 @@
+# Tested on black background
+# Source:
+# - https://github.com/EdenEast/nightfox.nvim/blob/main/lua/nightfox/palette/carbonfox.lua
+# - https://github.com/EdenEast/nightfox.nvim/blob/main/extra/carbonfox/carbonfox.fish
+
+set -g foreground f2f4f8 # 19.07:1, #f2f4f8
+set -g selection 2a2a2a #, #2a2a2a
+set -g comment 6e6f70 # , #6e6f70
+
+set -g red ee5396 # 6.31:1 (AA), #ee5396
+set -g orange 3ddbd9 # 12.33:1, #3ddbd9
+set -g yellow 08bdba # 8.99:1, #08bdba
+set -g green 25be6a # 8.65:1, #25be6a
+set -g purple be95ff # 8.93:1, #be95ff
+set -g cyan 33b1ff # 8.87:1, #33b1ff
+set -g pink 36a3d9 # 8.91:1, #ff7eb6
