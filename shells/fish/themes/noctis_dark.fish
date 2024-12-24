e8e15006f232a2c92ee31c20c8a6a3d4a5cb312c
Azzam S.A <noreply@azzamsa.com>
2024-12-24 10:07:45 +0700

    style: noctis dark

---
+++ shells/fish/themes/noctis_dark.fish
@@ -0,0 +1,17 @@
+# Noctis Hight Contrast
+#
+# Tested on black background
+# Source:
+# - https://github.com/KamenKolev/noctis-hc
+
+set -g foreground eceff4 # 16.03:1, #e6e1cf
+set -g selection 44475a #, #44475a
+set -g comment 60728a # , #888888
+
+set -g red ff669c # 7.63:1, #ff669c
+set -g orange ff7039 # 7.62:1, #ff7039
+set -g yellow e6ac00 # 10.26:1, #e6ac00
+set -g green 00cc76 # 9.89:1, #00cc76
+set -g purple a000ff # 3.93:1, #a000ff
+set -g cyan 99f3ff # 16.61:1, #99f3ff
+set -g pink 00a0ff # 7.47:1, #00a0ff
