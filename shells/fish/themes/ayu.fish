38e6fad59179405669016cd471a1618fed7ee7c7
Azzam S.A <noreply@azzamsa.com>
2024-12-25 02:50:01 +0700

    style: update ayu colors

--- shells/fish/themes/ayu.fish
+++ shells/fish/themes/ayu.fish
@@ -3,15 +3,16 @@
 # Tested on black background
 # Source:
 # - https://github.com/ayu-theme/ayu-vim/blob/master/colors/ayu.vim
+# - https://github.com/Shatur/neovim-ayu/blob/master/lua/ayu/colors.lua
 
 set -g foreground eceff4 # 16.03:1, #e6e1cf
 set -g selection 44475a #, #44475a
 set -g comment 60728a # , #5c6773
 
 set -g red f07178 # 7.33:1, #f07178
-set -g orange ff7733 # 7.93:1, #ff7733
-set -g yellow e7c547 # 12.47:1, #e7c547
-set -g green b8cc52 # 11.79:1, #b8cc52
-set -g purple ffee99 # 17.94:1, #ffee99
+set -g orange ff7733 # 9.25:1, #ff8f40
+set -g yellow e7c547 # 11.3:1, #e6b673
+set -g green b8cc52 # 12.73:1, #aad94c
+set -g purple ffee99 # 10.63:1, #d2a6ff
 set -g cyan 95e6cb # 14.45:1, #95e6cb
-set -g pink 36a3d9 # 7.39:1, #36a3d9
+set -g pink 36a3d9 # 9.33:1, #f29668
