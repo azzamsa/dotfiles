bbe3cbc92fbd8f75d39c053dffceda1d0737315c
Azzam S.A <noreply@azzamsa.com>
2024-12-24 08:32:52 +0700

    style: aura

---
+++ shells/fish/themes/aura.fish
@@ -0,0 +1,17 @@
+# Tested on black background
+# Source:
+# - https://github.com/rahil627/helix/blob/master/runtime/themes/aura-dark.toml
+# - https://github.com/daltonmenezes/aura-theme/tree/main/packages/color-palettes
+# - https://github.com/daltonmenezes/aura-theme/blob/main/packages/zed/themes/aura-dark.json
+
+set -g foreground edecee # 17.83:1, #edecee
+set -g selection 594a35 #, #594a35
+set -g comment 60728a # , #6d6d6d
+
+set -g red ff6767 # 7.38:1, #ff6767
+set -g orange ffca85 # 14.03:1, #ffca85
+set -g yellow ffca85 # 14.03:1, #ffca85
+set -g green 61ffca # 16.66:1, #61ffca
+set -g purple a277ff # 6.61:1 (AA), #a277ff
+set -g cyan 82e2ff # 14.27:1, #82e2ff
+set -g pink f694ff # 10.59:1, #f694ff
