d824b626663c87f514475970fc8ae08f003ef21a
Azzam S.A <noreply@azzamsa.com>
2024-12-25 10:31:00 +0700

    style: material theme

---
+++ shells/fish/themes/material.fish
@@ -0,0 +1,22 @@
+# Material Palenight
+#
+# Tested on black background
+# Source:
+# - https://github.com/ntpeters/base16-materialtheme-scheme/blob/master/material-palenight.yaml
+# - https://github.com/kaicataldo/material.vim/blob/main/colors/material.vim
+
+# default: #a6accd, 9.4:1
+set -g foreground eceff4 # 20.38:1, #eeffff
+set -g selection 343b51 #, #343b51
+set -g comment 676e95 # , #676e95
+
+set -g red ff5370 # 6.72:1 (AA), #ff5370
+set -g orange f78c6c # 8.92:1, #f78c6c
+set -g yellow ffcb6b # 14:1, #ffcb6b
+set -g green c3e88d # 15.24:1, #c3e88d
+set -g purple c792ea # 8.72:1, #c792ea
+set -g cyan 89ddff # 13.85:1, #89ddff
+set -g pink f07178 # 7.33:1, #f07178
+
+# Sadly, it becomes closed source now
+# See https://github.com/material-theme/vsc-material-theme/discussions/1279#discussioncomment-10722099
