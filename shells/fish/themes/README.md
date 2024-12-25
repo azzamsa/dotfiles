069fa5130571343a71ccd97ed04567c8e77941db
Azzam S.A <noreply@azzamsa.com>
2024-12-25 16:16:01 +0700

    style: snazzy

---
+++ shells/fish/themes/README.md
@@ -0,0 +1,8 @@
+# Themes
+
+Ranked by preference.
+
+- Snazzy
+- Dracula
+- Tokyonight Moon
+- Catppuccin Mocha
---
+++ shells/fish/themes/snazzy.fish
@@ -0,0 +1,24 @@
+# Tested on black background
+# Source:
+# - https://github.com/doomemacs/themes/blob/master/themes/doom-snazzy-theme.el
+# - https://github.com/sindresorhus/hyper-snazzy/blob/main/index.js
+
+set -g red ff5c57 # 6.92:1 (AA), #ff5c57
+set -g orange ffb86c # 12.32:1, #ffb86c (non OG)
+set -g yellow f3f99d # 18.84:1, #f3f99d
+set -g green 5af78e # 15.12:1, #5af78e
+set -g purple ff6ac1 # 8.08:1, #ff6ac1
+set -g cyan 9aedfe # 15.91:1, #9aedfe
+set -g pink 57c7ff # 11.01:1, #57c7ff
+
+#
+# Other colors
+# #bd93f9, 8.7:1
+# For purple, I use the one from `tokyonight_moon` as it is more readable: `#c099ff` (9.24:1)
+
+# All the colors below are kept accross theme.
+# Mostly taken from https://catppuccin.com/palette mocha.
+set -g selection 313244 #, #313244 (surface0)
+# OG Catppuccin Mocha uses overlay0
+set -g comment 7f849c # 5.68:1, #7f849c (overlay1)
+set -g foreground ffffff # 21:1, #ffffff
