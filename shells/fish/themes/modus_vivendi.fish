877fcdbd195c987b68b0805cf9b77c2b0be57da8
Azzam S.A <noreply@azzamsa.com>
2025-01-02 05:07:54 +0700

    style: use the `cooler` variant of modus theme

--- shells/fish/themes/modus_vivendi.fish
+++ shells/fish/themes/modus_vivendi.fish
@@ -1,14 +1,23 @@
+# Modus Vivendi
+#
 # Source:
 # - https://github.com/miikanissi/modus-themes.nvim/blob/master/extras/fish/modus_vivendi.fish
+# - https://github.com/protesilaos/modus-themes/blob/main/modus-vivendi-theme.el
 
-set -g red ff5f59 # 7.03:1, #ff5f59
-set -g orange fec43f # 13.18:1, #fec43f
-set -g yellow d0bc00 # 10.87:1, #d0bc00
-set -g green 44BC44 # 8.52:1, #44BC44
-set -g purple b6a0ff # 9.46:1, #b6a0ff
-set -g cyan 00d3d0 # 11.22:1, #00d3d0
-set -g pink feacd0 # 12.02:1, #feacd0
-set -g blue 2fafff # 8.69:1, #2fafff
+# set -g red ff5f59 # 7.03:1, #ff5f59
+# set -g yellow d0bc00 # 10.87:1, #d0bc00
+# set -g green 44bc44 # 8.52:1, #44bc44
+# set -g cyan 00d3d0 # 11.22:1, #00d3d0
+# set -g blue 2fafff # 8.69:1, #2fafff
+# set -g orange fec43f # 13.18:1, #fec43f
+
+set -g red ff7f86 # 8.63:1, #ff7f86 (red cooler)
+set -g green 00c06f # 8.76:1, #00c06f (green cooler)
+set -g yellow dfaf7a # 10.55:1, #dfaf7a (yellow cooler)
+set -g orange 00bcff # 9.63:1, #00bcff (blue cooler)
+set -g purple b6a0ff # 9.46:1, #b6a0ff (magenta cooler)
+set -g pink feacd0 # 12.02:1, #feacd0 (magenta)
+set -g cyan 6ae4b9 # 13.41:1, #6ae4b9 (cyan cooler)
 
 # All the colors below are kept accross theme.
 # Mostly taken from https://catppuccin.com/palette mocha.
