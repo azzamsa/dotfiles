a08bac0f9fea8014231ac6da9991b6def8d0831f
Azzam S.A <noreply@azzamsa.com>
2025-01-03 04:10:17 +0700

    refactor(wezterm): move theme to Lua for better flexibility

--- terms/wezterm/wezterm.lua
+++ terms/wezterm/wezterm.lua
@@ -2,11 +2,13 @@ require("configs.on")
 local core = require("configs.core")
 local keymap = require("configs.keymap")
 local ui = require("configs.ui")
+local colors = require("configs.colors")
 
 local config = {}
 
 core.append(config)
 ui.append(config)
 keymap.append(config)
+colors.append(config)
 
 return config
