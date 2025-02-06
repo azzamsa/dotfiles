473156fe2723e610ae7e4ae65c0544ae07341303
Azzam S.A <noreply@azzamsa.com>
2025-02-07 05:27:16 +0700

    build(snacks): upgrade dependencies

--- utils/snacks/src/utils.rs
+++ utils/snacks/src/utils.rs
@@ -2,8 +2,8 @@ use rand::Rng;
 use std::io::{self, Write};
 
 pub fn get_random_number(length: usize) -> usize {
-    let mut rng = rand::thread_rng();
-    rng.gen_range(0..length)
+    let mut rng = rand::rng();
+    rng.random_range(0..length)
 }
 
 pub fn stdout(input: &str) {
