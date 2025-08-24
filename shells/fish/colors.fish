0e7e6bccec80fcb860db4aaa5b5d62cfb9f4eb64
Azzam S.A <vcs@azzamsa.com>
2025-08-25 02:10:49 +0700

    style: reorder fields

--- shells/fish/colors.fish
+++ shells/fish/colors.fish
@@ -14,8 +14,8 @@ if [ $theme = modus_vivendi ]
     set -g yellow fec43f # 13.18:1 #fec43f
     set -g orange 2fafff # 8.85:1, #2fafff
     set -g purple b6a0ff # 9.46:1, #b6a0ff
-    set -g pink feacd0 # 12.02:1, #feacd0
     set -g cyan 00d3d0 # 13.42:1, #00d3d0
+    set -g pink feacd0 # 12.02:1, #feacd0
 
 else if [ $theme = tokyonight_moon ]
     # License: Apache 2.0
@@ -23,9 +23,9 @@ else if [ $theme = tokyonight_moon ]
     # - https://github.com/folke/tokyonight.nvim/blob/main/extras/fish/tokyonight_moon.fish
 
     set -g red ff757f # 8.1:1, #ff757f
-    set -g orange ff966c # 9.83:1, #ff966c
-    set -g yellow ffc777 # 13.68:1, #ffc777
     set -g green c3e88d # 15.24:1, #c3e88d
+    set -g yellow ffc777 # 13.68:1, #ffc777
+    set -g orange ff966c # 9.83:1, #ff966c
     set -g purple fca7ea # 11.85:1, #fca7ea
     set -g cyan 86e1fc # 14.18:1, #86e1fc
     set -g pink c099ff # 9.24:1, #c099ff
@@ -38,9 +38,9 @@ else if [ $theme = material_palenight ]
     # - https://github.com/t3dotgg/vsc-material-but-i-wont-sue-you/blob/main/scripts/generator/settings/specific/palenight-hc.ts
 
     set -g red f07178 # 7.33:1, #f07178
-    set -g orange f78c6c # 8.92:1, #f78c6c
-    set -g yellow ffcb6b # 14:1, #ffcb6b
     set -g green c3e88d # 15.24:1, #c3e88d
+    set -g yellow ffcb6b # 14:1, #ffcb6b
+    set -g orange f78c6c # 8.92:1, #f78c6c
     set -g purple c792ea # 8.72:1, #c792ea
     set -g cyan 89ddff # 13.85:1, #89ddff
     set -g pink ff9cac # 10.6:1, #ff9cac
@@ -51,9 +51,9 @@ else if [ $theme = dracula ]
     # - https://github.com/dracula/fish/blob/master/themes/Dracula%20Official.theme
 
     set -g red ff5555 # 6.68:1 (AA), #ff5555
-    set -g orange ffb86c # 12.32:1, #ffb86c
-    set -g yellow f1fa8c # 18.79:1, #f1fa8c
     set -g green 50fa7b # 15.3:1, #50fa7b
+    set -g yellow f1fa8c # 18.79:1, #f1fa8c
+    set -g orange ffb86c # 12.32:1, #ffb86c
     set -g purple bd93f9 # 8.7:1, #bd93f9
     set -g cyan 8be9fd # 15.17:1, #8be9fd
     set -g pink ff79c6 # 8.8:1, #ff79c6
@@ -64,9 +64,9 @@ else if [ $theme = catppuccin_mocha ]
     # - https://catppuccin.com/palette
 
     set -g red f38ba8 # 9.06:1, #f38ba8
-    set -g orange fab387 # 11.86:1, #fab387
-    set -g yellow f9e2af # 16.52:1, #f9e2af
     set -g green a6e3a1 # 14.12:1, #a6e3a1
+    set -g yellow f9e2af # 16.52:1, #f9e2af
+    set -g orange fab387 # 11.86:1, #fab387
     set -g purple cba6f7 # 10.33:1, #cba6f7
     set -g cyan 94e2d5 # 14.09:1, #94e2d5
     set -g pink f5c2e7 # 13.75:1, #f5c2e7
@@ -76,13 +76,12 @@ else if [ $theme = github ]
     # https://github.com/primer/primitives/blob/main/src/tokens/base/color/dark/dark.json5
 
     set -g red ff7b72 # 9.06:1, #ff7b72
-    set -g orange ffa657 # 11.86:1, #ffa657
-    set -g yellow f2cc60 # 16.52:1, #f2cc60
     set -g green 7ee787 # 14.12:1, #7ee787
+    set -g yellow f2cc60 # 16.52:1, #f2cc60
+    set -g orange ffa657 # 11.86:1, #ffa657
     set -g purple d2a8ff # 10.33:1, #d2a8ff
     set -g cyan 79c0ff # 14.09:1, #79c0ff
     set -g pink ffbedd # 13.75:1, #ffbedd
-
 end
 
 # All the colors below are kept across theme.
