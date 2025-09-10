38845babc129b3272d0cfc493fbe3b51bc0fa450
Azzam S.A <noreply@azzamsa.com>
2025-09-10 17:29:03 +0700

    docs: gsettings

--- _docs/post_install/apps.sh
+++ _docs/post_install/apps.sh
@@ -36,3 +36,23 @@ out rhythmbox xiterm+thai goldendict-ng hdate-applet fcitx5 mozc-utils-gui firef
 out wbrazilian witalian wfrench wspanish wswedish wcatalan wbulgarian wdanish wngerman wpolish wportuguese
 
 sudo nala autoremove && sudo nala autopurge
+
+#
+# Polish
+
+# GNOME Tweak
+gsettings set org.gnome.desktop.interface font-name "Atkinson Hyperlegible Next 13"
+gsettings set org.gnome.desktop.interface document-font-name "Atkinson Hyperlegible Next 13"
+gsettings set org.gnome.desktop.interface monospace-font-name "Maple Mono NF 13"
+gsettings set org.gnome.desktop.interface cursor-theme "Bibata-Modern-Ice"
+
+# Characters
+gsettings set org.gnome.Characters recent-characters "['🎉', '🏖️', '🧁', '🙏🏻']"
+
+# Keybindings
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>x']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>c']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>d']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>r']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications "[]"
