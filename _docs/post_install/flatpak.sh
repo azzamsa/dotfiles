0ed94386bfeceb85ff9673d0acc7573529e89139
Azzam S.A <noreply@azzamsa.com>
2025-09-11 22:13:47 +0700

    docs: more packages

--- _docs/post_install/flatpak.sh
+++ _docs/post_install/flatpak.sh
@@ -5,15 +5,16 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 #
 # Setup Core Packages
 set -l pkgs \
-    app.zen_browser.zen ca.desrt.dconf-editor com.audacityteam.Audacity com.brave.Browser \
+    app.zen_browser.zen ca.desrt.dconf-editor com.brave.Browser \
     com.calibre_ebook.calibre com.dec05eba.gpu_screen_recorder com.github.IsmaelMartinez.teams_for_linux \
     com.github.finefindus.eyedropper com.github.johnfactotum.Foliate com.github.qarmin.czkawka \
     com.github.tchx84.Flatseal com.mattjakeman.ExtensionManager com.usebruno.Bruno \
     dev.vencord.Vesktop io.dbeaver.DBeaverCommunity io.github.flattool.Warehouse \
     io.github.pieterdd.StretchBreak io.gitlab.news_flash.NewsFlash it.mijorus.gearlever \
     net.nokyan.Resources org.atheme.audacious org.flameshot.Flameshot org.gimp.GIMP \
-    org.gnome.Evolution org.gnome.Firmware org.gnome.World.PikaBackup org.inkscape.Inkscape \
-    org.keepassxc.KeePassXC org.kde.okular org.mozilla.Thunderbird org.telegram.desktop us.zoom.Zoom
+    com.audacityteam.Audacity org.gnome.Evolution org.gnome.Firmware org.gnome.World.PikaBackup \
+    org.inkscape.Inkscape org.keepassxc.KeePassXC org.kde.okular org.sqlitebrowser.sqlitebrowser \
+    org.mozilla.Thunderbird org.telegram.desktop us.zoom.Zoom
 
 for pkg in $pkgs
     flatpak install flathub --assumeyes $pkg
--- _docs/post_install/packages.sh
+++ _docs/post_install/packages.sh
@@ -19,13 +19,13 @@ set -l pkgs bash git fish curl wl-clipboard \
     # Rust
     clang mold libssl-dev \
     # Prompt
-    zoxide fmpeg 7zip poppler-utils imagemagick \
+    zoxide ffmpeg 7zip poppler-utils imagemagick \
     # Extensions
     ddcutil gir1.2-gda-5.0 gir1.2-gsound-1.0 \
     # Polish
     gnome-tweaks bibata-cursor-theme gnome-shell-pomodoro fastfetch \
     # pandoc will pull a whopping `199.2 MB` deps, because it so powerful it pull latex and everything.
-    jq fd-find ripgrep fzf telnet pandoc podman podman-compose shfmt \
+    jq fd-find ripgrep fzf telnet pandoc podman podman-compose shfmt markdown \
     # appimage
     libfuse-dev
 
